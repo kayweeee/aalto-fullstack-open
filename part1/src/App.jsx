fb6e2f88558130b1112437460afe98c7024131fd
kayweeee <90853438+kayweeee@users.noreply.github.com>
2023-09-13 15:37:34 +0300

    new: exercise 1.1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,47 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.content[0].part} {props.content[0].exercise}
+      </p>
+      <p>
+        {props.content[1].part} {props.content[1].exercise}
+      </p>
+      <p>
+        {props.content[2].part} {props.content[2].exercise}
+      </p>
+    </>
+  );
+};
+
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>;
+};
+
 const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+  const content = [
+    { part: part1, exercise: exercises1 },
+    { part: part2, exercise: exercises2 },
+    { part: part3, exercise: exercises3 },
+  ];
+  const total = exercises1 + exercises2 + exercises3;
+
   return (
     <div>
-      <p>Hello world</p>
+      <Header course={course} />
+      <Content content={content} />
+      <Total total={total} />
     </div>
   );
 };
